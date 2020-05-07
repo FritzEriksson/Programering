6a203c5df39e5ddede5e32b282a341c896cca2c3
FritzEriksson <h19frer@axevalla.fhsk.se>
2020-05-07 12:10:31 +0200

    Torsdag7/5

--- classer/Klass2.cpp
+++ classer/Klass2.cpp
@@ -3,102 +3,103 @@
 using namespace std;
 
 class Brawler{
-	public:
-		void setname();
-		void setHP();
-		void setMana();
-		class Spellbook{
-			public:
-				void castspell();
-				void choosespell();
-			private:
-				int Manacons;
-				int Dmg;
-		}Spells[3];
-	private:
-		String name;
-		int HP;
-		int Mana;
-}Brawler[2];
-
-void Brawler::Spellbook::choosespell(){
-	int x;
-	cout << "DU Öppnar :\nTobbe trollkarls Magilåda\nPick a card 1.\nPlaceholder 2.\nBenen på ryggen 3." << endl;
-	cin >> x;
-	void Brawler::Spellbook::Spells[x]::castspell();
-	
-}
-
-void Brawler::Spellbook::Spells::castspell(){
-	void Brawler[caster+1]::setHP();
-	void Brawler[caster]::setMana();
-	
-}
-
-void Brawler::setname(){
-	cout << "And on the right we have our contestor and i dont know his name... whats your name champion?" << endl;
-	cin >> z;
-	Brawler[0].name = z;
-	cout << endl << Brawler[0].name << endl;
-
-}
-
-bool rollhit(){
-	auto number = (rand() % 20) + 1;
-	if (number =< 7){
-		return (true);
-	else 
-		return (false);
-	}
-	
-}
-
-
-class Brawler{
-	public:
-		void setname();
-		void getname();
-		void spellcast();
-		void changebrawlerstats();
-		class spells
-			public:
-				void castspell();
-			private:
-				string spell;
-				int Manacost;
-				int dmg;
-	private:
-		string name;
-		int HP;
-		int Mana;
+  public:
+    void setname();
+    void changebrawlerstats();
+    class spells{
+      public:
+        void castspell();
+        int Manacost;
+        int dmg;
+      private:
+    }Spell[4];
+  private:
+    string name;
+    int HP;
+    int Mana;
 };
+bool rollhit(){
+  auto number = (rand() % 20) + 1;
+  if (number =< 7){
+    return (true);
+  else 
+    return (false);
+  }	
+}
 
 void Brawler::setname(){
-	cout << "..what's your name" << endl;
-	cin >> name;
-	cout << endl << "And on our left: The conterster" << name << endl;
+  cout << "..what's your name" << endl;
+  cin >> name;
+  cout << endl << "And on our left: The conterster" << name << endl;
 }
 
-bool rollhit(){
-	auto number = (rand() % 20) + 1;
-	if (number =< 7){
-		return (true);
-	else 
-		return (false);
-	}
-	
+void Brawler::changebrawlerstats(Manacost, Damage){
+  Mana = Mana - Manacost;
+  
 }
+
+void Brawler::Spells::castspell(){
+  if(rollhit() == true)
+    changebrawlerstats(Manacost, dmg);
+  else
+    cout << "The spell missed" << endl;
+}
+
+
 
 int main(){
-	Brawler::Spells Pickacard
-	Brawler::Spells Placeholder
-	Brawler::Spells Run;
-	
-	Brawler Player;
-	Brawler Enemy;
-	
-	cout << "Welcome to the Brawler guild! Two brave champions has made it to the finals! On the left: Our reigning champion KALGOOR!!!" << endl;
-	Player.setname();
+  Brawler::Spells Punch;
+    Punch.Manacost = 0;
+    Punch.dmg  = 5;
+  Brawler::Spells Pickacard;
+    Pickacard.Manacost = 10;
+    Pickacard.dmg  = 25;
+  Brawler::Spells Coinflip;
+    Coinflip.Manacost = 20;
+    Coinflip.dmg  = 30;
+  Brawler::Spells Vanish;
+    Vanish.Manacost = 30;
+    Vanish.dmg  = 0;
+  Brawler::Spells Swordtoss;
+    Swordtoss.Manacost = 10;
+    Swordtoss.dmg  = 25;
+  Brawler::Spells Leapofdoom;
+    Leap.Manacost = 10;
+    Leap.dmg  = 25;
+  Brawler::Spells Runinfear;
+    Runinfear.Manacost = 10;
+    Runinfear.dmg  = 25;
+  Brawler::Spells Brutalsmash;
+    Brutalsmash.Manacost = 0;
+    Brutalsmash.dmg  = 15;
+  Brawler::Spells Firestorm;
+    Firestrom.Manacost = 0;
+    Firestorm.dmg  = 25;
+  Brawler::Spells Killonsight;
+    Killonsight.Manacost = 0;
+    Killonsight.dmg  = 35;
+
+  Brawler Mage;
+  Brawler Warrior;
+  Brawler Enemy;
+  Mage.spell[0] = Punch;
+  Mage.spell[1] = Pickacard;
+  Mage.spell[2] = Coinflip;
+  Mage.spell[3] = Vanish;
+  Warrior.spell[0] = Punch;
+  Warrior.spell[1] = Swordtoss;
+  Warrior.spell[2] = Leapofdoom;
+  Warrior.spell[3] = Runinfear;
+  Enemy.spell[0] = Brutalsmash;
+  Enemy.spell[1] = Firestorm;
+  Enemy.spell[2] = Killonsight;
+  Enemy.spell[3] = Brutalsmash;
+  string player;
+  cout << "pick a class: Mage or Warrior?" << endl;
+  cin >> player;
+
+  cout << "Welcome to the Brawler guild! Two brave champions has made it to the finals! On the left: Our reigning champion KALGOOR!!!" << endl;
+  player.setname();
 }
 
 		
